e7c887da52d4d0e2769222c4629c85c1490ee48c
jwjb1020 <jwjb1020@gmail.com>
2023-11-08 18:30:09 +0900

    morgan으로 로그 관리

--- app/server.js
+++ app/server.js
@@ -4,14 +4,16 @@
 // 모듈
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
+
 const app = express();
+dotenv.config();
 //이전방식 현재는 express에 내장되어있음
 // const bodyParser = require("body-parser");
 
 
 const home = require("./src/routes/home");
-
+const accessLogStream =require("./src/config/log")
 
 //앱 세팅
 app.set("views", "./src/views");
@@ -21,6 +23,8 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(express.urlencoded({ extended :true }));
+app.use(morgan("dev"))
+app.use(morgan("common", {stream : accessLogStream}));
 
 // use -> 미들 웨어를 등록해주는 매서드
 //순서가 중요함!
